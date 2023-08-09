bf2e4a65146116e4d2fad12df5ca98892958c55d
Centeno448 <diegocenteno448@gmail.com>
2023-08-08 21:31:33 -0500

    added support for test logs

--- tests/events/helpers.rs
+++ tests/events/helpers.rs
@@ -1,7 +1,22 @@
 use actix::{Actor, Context, Handler};
 use network_tic_tac_toe::game_server::{GameRoomStatus, GameServer, ServerMessage};
+use network_tic_tac_toe::telemetry::{get_subscriber, init_subscriber};
+use once_cell::sync::Lazy;
 use std::sync::{atomic::AtomicUsize, Arc};
 use uuid::Uuid;
+
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    }
+});
 
 pub struct MockPlayerSession;
 
@@ -32,6 +47,7 @@ pub fn get_player_ids_from_room(game_server: &GameServer, room_name: &str) -> Ve
 }
 
 pub fn setup_empty_game_server() -> GameServer {
+    Lazy::force(&TRACING);
     let visitors = Arc::new(AtomicUsize::new(0));
     GameServer::new(visitors)
 }
