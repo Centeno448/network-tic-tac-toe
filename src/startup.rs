7da4e85222221fe36539d591c843586edb87ec9b
Centeno448 <diegocenteno448@gmail.com>
2023-08-16 22:50:38 -0500

    misc: refactor startup to use listener

--- src/startup.rs
+++ src/startup.rs
@@ -1,10 +1,10 @@
-use std::sync::atomic::AtomicUsize;
-use std::sync::Arc;
-
 use actix::{Actor, Addr};
 use actix_web::dev::Server;
 use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
 use actix_web_actors::ws;
+use std::net::TcpListener;
+use std::sync::atomic::AtomicUsize;
+use std::sync::Arc;
 use tracing_actix_web::TracingLogger;
 use uuid::Uuid;
 
@@ -21,8 +21,12 @@ pub struct ApplicationBaseUrl(pub String);
 
 impl Application {
     pub async fn build(configuration: ApplicationSettings) -> Result<Self, anyhow::Error> {
-        let port = configuration.port;
-        let server = run(configuration.host, configuration.port).await?;
+        let address = format!("{}:{}", configuration.host, configuration.port);
+        let listener = TcpListener::bind(address.clone())?;
+
+        let port = listener.local_addr().unwrap().port();
+
+        let server = run(listener).await?;
 
         Ok(Self { port, server })
     }
@@ -54,7 +58,7 @@ pub async fn index(
     resp
 }
 
-pub async fn run(host: String, port: u16) -> Result<Server, anyhow::Error> {
+pub async fn run(listener: TcpListener) -> Result<Server, anyhow::Error> {
     let app_state = Arc::new(AtomicUsize::new(0));
     let game_server = GameServer::new(app_state.clone()).start();
 
@@ -64,7 +68,7 @@ pub async fn run(host: String, port: u16) -> Result<Server, anyhow::Error> {
             .app_data(web::Data::new(game_server.clone()))
             .route("/", web::get().to(index))
     })
-    .bind((host, port))?
+    .listen(listener)?
     .run();
 
     Ok(server)
